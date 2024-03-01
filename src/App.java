d2c3e6bbf2a1085a2b2208a6410a21beb9fef384
Nova <nb.nova.wise@gmail.com>
2024-03-01 13:14:16 -0500

    Added minions response

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Minions! Tonight, we steal the moon!");
+        System.out.println("Yeeeeeeaaaaaaaaaahhhhhhhhhh");
     }
 }
